439d26c1825c582fdf17821f14cd39eb4bf9006a
Alexander <alexander_j_garcia@estudante.sesisenai.org.br>
2026-06-22 20:56:00 -0300

    Controle de idade

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
-﻿// QUESTÃO 1: Média de notas
+﻿// Questão 2: Controle de idade da turma
 
-double[] notas = new double[5];
-double soma = 0;
+int[] idades = new int[5];
+int menores = 0;
+int maiores = 0;
 
-// Preenchimento do vetor
-for (int i = 0; i < 5; i++)
+// Preenchimento do vetor e contagem
+for (int i = 0; i < 8; i++)
 {
-    Console.Write("Informe a nota: ");
-    notas[i] = double.Parse(Console.ReadLine());
-    soma += notas[i];
+    Console.Write("Informe a idade do estudante: ");
+    idades[i] = int.Parse(Console.ReadLine()!);
+
+    if (idades[i] < 18)
+    {
+        menores++;
+    }
+    else
+    {
+        maiores++;
+    }
 }
 
-// Cálculo da média
-double media = soma / 5;
-
-// Saída com vírgula como separador decimal
-Console.WriteLine($"Soma das notas: {soma.ToString().Replace(".", ",")}");
-Console.WriteLine($"Média final: {media.ToString().Replace(".", ",")}");
+// Saída
+Console.WriteLine($"quantidade de menores de idade: {menores}");
+Console.WriteLine($"quantidade de maiores de idade: {maiores}");
